a9d65a5731ed3ac79e9ac8146ae7142f7602b2ea
Vipirak <andreas.l.gade@gmail.com>
2017-11-14 14:23:25 +0100

    First Proper Commit

---
+++ Server/Server/ConnectionHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal static class ConnectionHandler
+    {
+        private static int port;
+
+        public static int Port
+        {
+            get
+            {
+                return port;
+            }
+            set
+            {
+                port = value;
+            }
+        }
+
+        private static Dictionary<IPAddress, Socket> connections;
+
+        static ConnectionHandler()
+        {
+            ConnectionHandler.connections = new Dictionary<IPAddress, Socket>();
+        }
+
+        public static void Init(int port)
+        {
+            ConnectionHandler.port = port;
+        }
+    }
+}
