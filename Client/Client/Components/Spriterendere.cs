0af6f4f8a3e347a69f30c24493f4d7f22076c36f
Nikolaj Vestergaard Hentze <niko0654@student.eadania.dk>
2017-11-16 10:31:43 +0100

    12

--- Client/Client/Components/Spriterendere.cs
+++ Client/Client/Components/Spriterendere.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Client
 {
-    class Spriterendere
+    class Spriterendere : com
     {
         private Rectangle rectangle;
         private Texture2D sprite;
