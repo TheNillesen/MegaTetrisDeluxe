3d35e563eebe2da314cbc808927a4cc07fcf5172
Nikolaj Vestergaard Hentze <niko0654@student.eadania.dk>
2017-11-16 10:24:57 +0100

    7

--- Client/Client/Components/Spriterendere.cs
+++ Client/Client/Components/Spriterendere.cs
@@ -8,5 +8,64 @@ namespace Client
 {
     class Spriterendere
     {
+        private Rectangle rectangle;
+        private Texture2D sprite;
+        private string spriteName;
+        private float layerDepth;
+        public Vector2 Offset { get; set; }
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return rectangle;
+            }
+
+            set
+            {
+                rectangle = value;
+            }
+        }
+
+        public Texture2D Sprite
+        {
+            get
+            {
+                return sprite;
+            }
+
+            set
+            {
+                sprite = value;
+            }
+        }
+
+        public Spriterendere(GameObject gameObject, string spriteName, float layerDepth) : base(gameObject)
+        {
+            this.spriteName = spriteName;
+            this.layerDepth = layerDepth;
+
+        }
+
+        public void update()
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>(spriteName);
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, GetGameObject().Transform.Position + Offset, Rectangle, Color.White);
+        }
+
     }
+
+
+
+
 }
+
