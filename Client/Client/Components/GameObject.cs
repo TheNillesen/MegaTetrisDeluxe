128f2d9d2cc480a48b4a6b7d32e285b34f70d89e
Satire Satyr <andreas.l.gade@gmail.com>
2017-11-16 10:33:35 +0100

    Awesome Gameobject things

--- Client/Client/Components/GameObject.cs
+++ Client/Client/Components/GameObject.cs
@@ -14,5 +14,31 @@ namespace Client
         public Transform Transform { get { return transform; } }
         public Spriterendere Rendere { get { return renderer; } }
 
+        private List<Component> components;
+
+        public GameObject()
+        {
+            components = new List<Component>();
+        }
+
+        public T GetComponent<T>() where T : Component
+        {
+            return components.Find(o => o is T) as T;
+        }
+
+        public Component GetComponent(Predicate<Component> filter)
+        {
+            return components.Find(filter);
+        }
+
+        public T GetComponent<T>(Predicate<T> filter) where T : Component
+        {
+            return components.Find(o => o is T && filter(o as T)) as T;
+        }
+        
+        public T[] GetComponents<T>() where T : Component
+        {
+            return (from Component comp in components where comp is T select comp as T).ToArray();
+        }
     }
 }
