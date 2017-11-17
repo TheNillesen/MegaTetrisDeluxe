4184efb47840cb1036bf68e25c52579cbb8fecd3
Anders Vestergaard <mufas.anders@hotmail.com>
2017-11-17 10:53:35 +0100

    gameMap initialized in gameworld

--- Client/Client/Components/PlayerController.cs
+++ Client/Client/Components/PlayerController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Client
 {
-    class PlayerController : Component
+    class PlayerController : Component, IUpdatable
     {
+        KeyboardState keyCurrent;
+        KeyboardState keyLast;
+
         public PlayerController(GameObject gameObject) : base(gameObject)
         {
+            keyLast = Keyboard.GetState();
+            keyLast = Keyboard.GetState();
+        }
 
+        public void Update()
+        {
+
+
+
+            //if (state.IsKeyDown(Keys.Right))
+            //    gameObject.Transform.MoveRight();
+            //if (state.IsKeyDown(Keys.Left))
+            //    gameObject.Transform.MoveLeft();
         }
     }
 }
