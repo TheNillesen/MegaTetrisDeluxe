89b9c9b51d30c7e06b4a055d0eaa58a12140b2ac
Anders Vestergaard <mufas.anders@hotmail.com>
2017-11-17 13:28:58 +0100

    SourceRectangle fixed

--- Client/Client/Components/Spriterendere.cs
+++ Client/Client/Components/Spriterendere.cs
@@ -59,7 +59,7 @@ namespace Client
         public void LoadContent(ContentManager content)
         {
             sprite = content.Load<Texture2D>(spriteName);
-
+            rectangle = new Rectangle(0, 0, sprite.Width, sprite.Height);
         }
         public void Draw(SpriteBatch spriteBatch)
         {//Er ikke helt færdig den skal tage fat i gameobject.transfrom når transform er færdig..
