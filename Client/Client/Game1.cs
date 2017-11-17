56aacc3bf0089c461f1cc30aef05741967993e40
Anders Vestergaard <mufas.anders@hotmail.com>
2017-11-17 11:38:15 +0100

    updates and draws gameobjects. Now there is a list of Gameobjects and a variable holding the player in gameworld.

--- Client/Client/Game1.cs
+++ Client/Client/Game1.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Anders.Vestergaard;
 using Andreas.Gade;
+using System.Collections.Generic;
 
 namespace Client
 {
@@ -27,8 +28,10 @@ namespace Client
 
         private bool hasRun = false;
         private DateTime startup = DateTime.Now;
+        private List<GameObject> gameObjects;     
 
         public GameMap gameMap;
+        public GameObject player;
 
         private Gameworld()
         {
@@ -52,15 +55,14 @@ namespace Client
             GameClient gc = new GameClient();
             new System.Threading.Thread(() => gc.Connect(new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 }), 6666)).Start();
 #endif
+            gameObjects = new List<GameObject>();
             gameMap = new GameMap(20, 20, 100, 100, new Vector2(0, 0));
 
             //Test player
-            GameObject player = new GameObject();
+            player = new GameObject();
             player.AddComponent(new Spriterendere(player, "GreyToneBlock.png", 1f));
             player.AddComponent(new Transform(player, new Vector2(5, 5)));
             player.AddComponent(new PlayerController(player));
-            player.LoadContent(this.Content);
-
         }
 
         /// <summary>
@@ -71,6 +73,8 @@ namespace Client
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player.LoadContent(this.Content);
 
             // TODO: use this.Content to load your game content here
         }
@@ -95,6 +99,9 @@ namespace Client
                 Exit();
 
             // TODO: Add your update logic here
+            for (int i = 0; i < gameObjects.Count; i++)
+                gameObjects[i].Update();
+            player.Update();
 
             base.Update(gameTime);
 
@@ -110,6 +117,9 @@ namespace Client
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            for (int i = 0; i < gameObjects.Count; i++)
+                gameObjects[i].Draw(spriteBatch);
+            player.Draw(spriteBatch);
 
             base.Draw(gameTime);
         }
