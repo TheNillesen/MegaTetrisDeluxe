f39e8206a0dde01b9fbf462c824e9bcdbe6ce73f
Nikolaj Vestergaard Hentze <niko0654@student.eadania.dk>
2017-11-17 10:40:26 +0100

    asd

--- Client/Client/Components/Spriterendere.cs
+++ Client/Client/Components/Spriterendere.cs
@@ -63,7 +63,7 @@ namespace Client
         }
         public void Draw(SpriteBatch spriteBatch)
         {//Er ikke helt færdig den skal tage fat i gameobject.transfrom når transform er færdig.
-            spriteBatch.Draw(sprite, gameObject.Transform.Position(), Rectangle, Color.White);
+            spriteBatch.Draw(sprite, Transform.Position, Rectangle, Color.White);
         }
 
     }
