bc32a0475fb63919102abe345966547e7990aea7
Vipirak <andreas.l.gade@gmail.com>
2017-11-17 11:49:59 +0100

    Server side game stuff

---
+++ Server/Intermediate/Enum/GameShapes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Enum
+{
+    public enum GameShapes : byte
+    {
+        L = 0x00,
+        T = 0x01,
+        I = 0x02,
+        Square = 0x03,
+        Lightning = 0x04,
+        Lightning_Inverse = 0x05,
+        L_Inverse = 0x06
+    }
+}
---
+++ Server/Server/GameLogic/Grid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Grid
+    {
+        private int width;
+        private int height;
+        private List<GameObject> objects;
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+
+        public List<GameObject> Objects
+        {
+            get { return objects; }
+        }
+
+        public Grid(int width, int height)
+        {
+
+        }
+    }
+}
