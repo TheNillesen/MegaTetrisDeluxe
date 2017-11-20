cd351ae517d10d71b37481f6d0a2973183ddd2f7
Anders Vestergaard <mufas.anders@hotmail.com>
2017-11-20 14:11:31 +0100

    Now you can rotate (use Q and E) and a function for random shape has been made, which is used to make a random start shape.

--- Client/Client/Components/Transform.cs
+++ Client/Client/Components/Transform.cs
@@ -17,26 +17,26 @@ namespace Client
         public Vector2[] Position { get; set; }
         // Shapes: I, L, Lightning, Lightning_invers, L_invers, Square, T, Polygon
         public GameShapes shape;
-        public Vector2I[] ShapeCord
-        {
-            get
-            {
-                return GameShapeHelper.GetShape(shape);
-            }
-        }
+        public Vector2[] ShapeCord;
+
         private float angle;
 
         public Transform(GameObject gameObject, Vector2 position) : base(gameObject)
         {
             this.Position = new Vector2[4];
 
+            //For test, I have given a standard shape
+            Shape();
+            angle = 0f;
+            //The shape's coordinates
+            Vector2I[] tempShapeCord = GameShapeHelper.GetShape(shape);
+            ShapeCord = new Vector2[4];
+            for(int i = 0; i < ShapeCord.Count(); i++)
+                ShapeCord[i] = new Vector2(tempShapeCord[i].X, tempShapeCord[i].Y);
+
             Vector2 tempPos = Gameworld.Instance.gameMap.MapPosition(position);
             for (int i = 0; i < Position.Count(); i++)
                 Position[i] = Gameworld.Instance.gameMap.Position(new Vector2(tempPos.X + ShapeCord[i].X, tempPos.Y + ShapeCord[i].Y));
-
-            //For test, I have given a standard shape
-            shape = GameShapes.Lightning;
-            angle = 0f;
         }
         public void Translate(Vector2 translation)
         {
@@ -49,56 +49,112 @@ namespace Client
         /// </summary>
         public void MoveRight()
         {
-            Vector2 tempPos = Gameworld.Instance.gameMap.MapPosition(Position[0]);
-
-            if (!Gameworld.Instance.gameMap.IsOutOfBound(new Vector2(tempPos.X + 1, tempPos.Y), shape) && Gameworld.Instance.gameMap.IsItOccupied(new Vector2 (tempPos.X + 1, tempPos.Y), shape) == false)
-            {
-                Gameworld.Instance.gameMap.EmptyPosition(tempPos, shape);
-                tempPos += new Vector2(1, 0);
-                Gameworld.Instance.gameMap.PlaceGameObject(gameObject, tempPos, shape);
-                for(int i = 0; i < Position.Count(); i++)
-                    Position[i] = Gameworld.Instance.gameMap.Position(new Vector2(tempPos.X + ShapeCord[i].X, tempPos.Y + ShapeCord[i].Y));
-            }
+            Move(new Vector2(1, 0));
         }
         /// <summary>
         /// moves the character left
         /// </summary>
         public void MoveLeft()
         {
-            Vector2 tempPos = Gameworld.Instance.gameMap.MapPosition(Position[0]);
-
-                if (!Gameworld.Instance.gameMap.IsOutOfBound(new Vector2(tempPos.X - 1, tempPos.Y), shape) && Gameworld.Instance.gameMap.IsItOccupied(new Vector2(tempPos.X - 1, tempPos.Y), shape) == false)
-                {
-                    Gameworld.Instance.gameMap.EmptyPosition(tempPos, shape);
-                    tempPos += new Vector2(-1, 0);
-                    Gameworld.Instance.gameMap.PlaceGameObject(gameObject, tempPos, shape);
-                    for (int i = 0; i < Position.Count(); i++)
-                        Position[i] = Gameworld.Instance.gameMap.Position(new Vector2(tempPos.X + ShapeCord[i].X, tempPos.Y + ShapeCord[i].Y));
-            }
+            Move(new Vector2(-1, 0));
         }
         /// <summary>
         /// moves the character one tile down.
         /// </summary>
         public void MoveDown()
         {
+            Move(new Vector2(0, 1));
+        }
+
+        private void Move(Vector2 move)
+        {
             Vector2 tempPos = Gameworld.Instance.gameMap.MapPosition(Position[0]);
 
-            if (!Gameworld.Instance.gameMap.IsOutOfBound(new Vector2(tempPos.X, tempPos.Y + 1), shape) && Gameworld.Instance.gameMap.IsItOccupied(new Vector2(tempPos.X, tempPos.Y + 1), shape) == false)
+            if (!Gameworld.Instance.gameMap.IsOutOfBound(new Vector2(tempPos.X, tempPos.Y) + move, ShapeCord) && Gameworld.Instance.gameMap.IsItOccupied(new Vector2(tempPos.X, tempPos.Y) + move, ShapeCord) == false)
             {
-                Gameworld.Instance.gameMap.EmptyPosition(tempPos, shape);
-                tempPos += new Vector2(0, 1);
-                Gameworld.Instance.gameMap.PlaceGameObject(gameObject, tempPos, shape);
+                Gameworld.Instance.gameMap.EmptyPosition(tempPos, ShapeCord);
+                tempPos += move;
+                Gameworld.Instance.gameMap.PlaceGameObject(gameObject, tempPos, ShapeCord);
                 for (int i = 0; i < Position.Count(); i++)
                     Position[i] = Gameworld.Instance.gameMap.Position(new Vector2(tempPos.X + ShapeCord[i].X, tempPos.Y + ShapeCord[i].Y));
             }
         }
 
         /// <summary>
-        /// Roteates the character left.
+        /// Roteates the character 90 degrees left.
         /// </summary>
         public void RotateLeft()
         {
+            Rotate(-90);
+        }
 
+        /// <summary>
+        /// Roteates the character 90 degrees right.
+        /// </summary>
+        public void RotateRight()
+        {
+            Rotate(90);
+        }
+
+        private void Rotate(float rotationAngle)
+        {
+            Vector2[] temp = ShapeCord;
+            for (int i = 1; i < temp.Count(); i++)
+                temp[i] = RotPointsAroundPointMath.RotatePoint(temp[i], temp[0], rotationAngle);
+
+            Vector2 tempPos = Gameworld.Instance.gameMap.MapPosition(Position[0]);
+
+            if (!Gameworld.Instance.gameMap.IsOutOfBound(tempPos, temp) && Gameworld.Instance.gameMap.IsItOccupied(tempPos, temp) == false)
+            {
+                ShapeCord = temp;
+                Gameworld.Instance.gameMap.EmptyPosition(tempPos, ShapeCord);
+                Gameworld.Instance.gameMap.PlaceGameObject(gameObject, tempPos, ShapeCord);
+                for (int i = 0; i < Position.Count(); i++)
+                    Position[i] = Gameworld.Instance.gameMap.Position(new Vector2(tempPos.X + ShapeCord[i].X, tempPos.Y + ShapeCord[i].Y));
+            }
+        }
+
+        /// <summary>
+        /// Sets a random shape.
+        /// </summary>
+        public void Shape()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 8);
+            //Their are 7 shapes in total.
+            switch (num)
+            {
+                case 1:
+                    shape = GameShapes.I;
+                    break;
+                case 2:
+                    shape = GameShapes.L;
+                    break;
+                case 3:
+                    shape = GameShapes.Lightning;
+                    break;
+                case 4:
+                    shape = GameShapes.Lightning_Inverse;
+                    break;
+                case 5:
+                    shape = GameShapes.L_Inverse;
+                    break;
+                case 6:
+                    shape = GameShapes.Square;
+                    break;
+                case 7:
+                    shape = GameShapes.T;
+                    break;
+                default:
+                    shape = GameShapes.I;
+                    break;
+            }
+
+            //The shape's coordinates
+            Vector2I[] tempShapeCord = GameShapeHelper.GetShape(shape);
+            ShapeCord = new Vector2[4];
+            for (int i = 0; i < ShapeCord.Count(); i++)
+                ShapeCord[i] = new Vector2(tempShapeCord[i].X, tempShapeCord[i].Y);
         }
 
         public void OnTick()
